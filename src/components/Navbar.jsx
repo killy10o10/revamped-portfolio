041b6ac0b430a015caaf892cfea25a9987910bc3
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-02-10 03:33:22 +0000

    Add navbar
    Setup project to use sass

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h1>Quami Killy</h1>
+      </div>
+      <div className="menu">
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">About Me</a>
+          <a href="/">Contact</a>
+        </nav>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
